9b583ad55d6b8df7b1e618afd53576164ac4f77a
cyberspidy1 <36754635+cyberspidy1@users.noreply.github.com>
2022-03-09 14:59:25 +0530

    Now you set desired output filename

--- nmaphtmlreport.py
+++ nmaphtmlreport.py
@@ -4,11 +4,12 @@ import subprocess
 command = 'nmap'
 target = input ("Enter target:")
 filename = input("Enter file name:")
+outputfilename = input("enter output filename with .html:")
 print ('processing.....')
 file_ = open('nmap_output.txt', 'w+')
 subprocess.run([command, '-A', '-Pn', target, '-oX', filename ], stdout=file_)
 print ('completed')
 file_.close()
-f = open('nmap_output.html', 'w+')
+f = open(outputfilename, 'w+')
 subprocess.run(['xsltproc', filename,], stdout=f)
 f.close()
